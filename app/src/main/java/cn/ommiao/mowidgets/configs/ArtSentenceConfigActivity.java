513a248992f3b6144846405fbd3037661837005c
oMMiao <ommaio@foamail.com>
2019-08-03 20:11:33 +0800

    Add widget 'Art Sentence'.

---
+++ app/src/main/java/cn/ommiao/mowidgets/configs/ArtSentenceConfigActivity.java
@@ -0,0 +1,108 @@
+package cn.ommiao.mowidgets.configs;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+import cn.ommiao.mowidgets.R;
+import cn.ommiao.mowidgets.databinding.LayoutColorSelectorBinding;
+import cn.ommiao.mowidgets.databinding.LayoutEdittextBinding;
+import cn.ommiao.mowidgets.utils.SPUtil;
+import cn.ommiao.mowidgets.utils.StringUtil;
+import cn.ommiao.mowidgets.utils.ToastUtil;
+import cn.ommiao.mowidgets.widgets.ArtSentenceWidget;
+
+public class ArtSentenceConfigActivity extends BaseConfigActivity<ArtSentenceWidget> {
+
+    private LayoutColorSelectorBinding selectorBindingL, selectorBindingS;
+    private String colorL, colorS;
+
+    private LayoutEdittextBinding edittextBindingL, edittextBindingS;
+    private String textL, textS;
+
+    private LayoutEdittextBinding offsetBinding;
+    private String offset;
+
+    @Override
+    protected ArtSentenceWidget getTargetWidget() {
+        return new ArtSentenceWidget();
+    }
+
+    @Override
+    protected void initConfigViews() {
+        edittextBindingL = getEdittextBinding("文字内容(大)", 5);
+        edittextBindingS = getEdittextBinding("文字内容(小)", 30);
+        edittextBindingL.et.setText("爱情");
+        edittextBindingS.et.setText("真的是一场龙卷风啊");
+        addConfigView(edittextBindingL.getRoot());
+        addConfigView(edittextBindingS.getRoot());
+        selectorBindingL = getColorSelectorBinding("文字颜色(大)");
+        selectorBindingS = getColorSelectorBinding("文字颜色(小)");
+        addConfigView(selectorBindingL.getRoot());
+        addConfigView(selectorBindingS.getRoot());
+        offsetBinding = getNumberEdittextBinding("小字偏移");
+        offsetBinding.et.setHint("正值向下，负值向上偏移，不填为默认位置");
+        offsetBinding.et.setFilters(new InputFilter[]{
+                new InputFilter.LengthFilter(4),
+                (source, start, end, dest, dstart, dend) -> {
+                    String allDigits = "-1234567890";
+                    String result = source.toString();
+                    for(int i = end - 1; i >= start; i--){
+                        String c = String.valueOf(source.charAt(i));
+                        if(!allDigits.contains(c)){
+                            result = result.replace(c, "");
+                        }
+                    }
+                    return result;
+                }
+        });
+        addConfigView(offsetBinding.getRoot());
+    }
+
+    @Override
+    protected String getConfigTitle() {
+        return getString(R.string.title_art_sentence_settings);
+    }
+
+    @Override
+    protected boolean isDataValid() {
+        textL = edittextBindingL.et.getText().toString().trim();
+        textS = edittextBindingS.et.getText().toString().trim();
+        colorL = selectorBindingL.etColor.getText().toString();
+        colorS = selectorBindingS.etColor.getText().toString();
+        offset = offsetBinding.et.getText().toString().trim();
+        if(StringUtil.isEmpty(offset)){
+            offset = "0";
+        }
+        if(StringUtil.isEmpty(textL)){
+            ToastUtil.shortToast("请输入文字内容(大)");
+            return false;
+        }
+        if(StringUtil.isEmpty(textS)){
+            ToastUtil.shortToast("请输入文字内容(小)");
+            return false;
+        }
+        if(!isColorValid(colorL)){
+            ToastUtil.shortToast("请输入有效的文字颜色(大)");
+            return false;
+        }
+        if(!isColorValid(colorS)){
+            ToastUtil.shortToast("请输入有效的文字颜色(小)");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void saveConfigs() {
+        SPUtil.put(getString(R.string.label_art_sentence) + widgetId + "_text_large", textL);
+        SPUtil.put(getString(R.string.label_art_sentence) + widgetId + "_text_small", textS);
+        SPUtil.put(getString(R.string.label_art_sentence) + widgetId + "_color_large", "#" + colorL);
+        SPUtil.put(getString(R.string.label_art_sentence) + widgetId + "_color_small", "#" + colorS);
+        SPUtil.put(getString(R.string.label_art_sentence) + widgetId + "_offset", offset);
+    }
+
+    @Override
+    protected boolean isSharedWidget() {
+        return true;
+    }
+}
---
+++ app/src/main/java/cn/ommiao/mowidgets/widgets/ArtSentenceWidget.java
@@ -0,0 +1,101 @@
+package cn.ommiao.mowidgets.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.text.TextPaint;
+import android.widget.RemoteViews;
+
+import cn.ommiao.mowidgets.R;
+import cn.ommiao.mowidgets.utils.SPUtil;
+
+public class ArtSentenceWidget extends BaseWidget {
+
+    private String textL, textS;
+    private int colorL, colorS;
+    private int sizeL, sizeS;
+    private int offset;
+
+    @Override
+    public RemoteViews getRemoteViews(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_art_sentence);
+        textL = SPUtil.getString(context.getString(R.string.label_art_sentence) + appWidgetId + "_text_large", "");
+        textS = SPUtil.getString(context.getString(R.string.label_art_sentence) + appWidgetId + "_text_small", "");
+        colorL = Color.parseColor(SPUtil.getString(context.getString(R.string.label_art_sentence) + appWidgetId + "_color_large", "#ffffff"));
+        colorS = Color.parseColor(SPUtil.getString(context.getString(R.string.label_art_sentence) + appWidgetId + "_color_small", "#ffffff"));
+        sizeL = context.getResources().getDimensionPixelSize(R.dimen.artSentenceLargeSize);
+        sizeS = context.getResources().getDimensionPixelSize(R.dimen.artSentenceSmallSize);
+        offset = Integer.parseInt(SPUtil.getString(context.getString(R.string.label_art_sentence) + appWidgetId + "_offset", "0"));
+        views.setImageViewBitmap(R.id.iv_text, getBitmap());
+        return views;
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    private Bitmap getBitmap() {
+
+        int bitmapWidth = getTextFullWidth(), bitmapHeight = 500;
+        Bitmap bitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        //canvas.drawColor(Color.BLACK);
+
+        Paint paintL = new Paint();
+        paintL.setAntiAlias(true);
+        paintL.setColor(colorL);
+        paintL.setTextSize(sizeL);
+
+        canvas.drawText(textL, getTextFullWidth() / 2 - getTextWidthL() / 2, bitmapHeight / 2 + Math.abs(paintL.ascent() + paintL.descent()) / 2, paintL);
+
+        Paint paintS = new Paint();
+        paintS.setAntiAlias(true);
+        paintS.setColor(colorS);
+        paintS.setTextSize(sizeS);
+
+        int rectHeight = getTextHeight(paintS) + 10;
+        Rect rect = new Rect();
+        rect.left = 0;
+        rect.right = getTextFullWidth();
+        rect.top = bitmapHeight / 2 + offset;
+        rect.bottom = rect.top + rectHeight;
+
+        canvas.drawRect(rect, paintL);
+
+        paintL.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.XOR));
+
+        canvas.drawRect(rect, paintL);
+
+        canvas.drawText(textS, getTextFullWidth() / 2 - getTextWidthS() / 2, rect.top + rectHeight / 2 + Math.abs(paintS.ascent() + paintS.descent()) / 2, paintS);
+
+        return bitmap;
+    }
+
+    private int getTextFullWidth(){
+        int width = getTextWidthL() > getTextWidthS() ?
+                getTextWidthL() : getTextWidthS();
+        return width > 0 ? width : 600;
+    }
+
+    private int getTextWidthL(){
+        TextPaint textPaint = new TextPaint();
+        textPaint.setTextSize(sizeL);
+        return (int) textPaint.measureText(textL);
+    }
+
+    private int getTextWidthS(){
+        TextPaint textPaint = new TextPaint();
+        textPaint.setTextSize(sizeS);
+        return (int) textPaint.measureText(textS);
+    }
+
+    private int getTextHeight(Paint paint){
+        //文字基准线的下部距离-文字基准线的上部距离 = 文字高度
+        return (int) (paint.descent() - paint.ascent());
+    }
+
+}
