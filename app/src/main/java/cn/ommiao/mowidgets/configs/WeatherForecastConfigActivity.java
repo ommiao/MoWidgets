50d4f56d2e903723217e20e749dbf08f1226cb91
likai <ommiao@foxmail.com>
2019-10-09 13:18:04 +0800

    Optimize widget 'Weather Forecast', and add preview.

--- app/src/main/java/cn/ommiao/mowidgets/configs/WeatherForecastConfigActivity.java
+++ app/src/main/java/cn/ommiao/mowidgets/configs/WeatherForecastConfigActivity.java
@@ -3,6 +3,7 @@ package cn.ommiao.mowidgets.configs;
 import android.text.InputFilter;
 
 import cn.ommiao.mowidgets.R;
+import cn.ommiao.mowidgets.databinding.LayoutColorSelectorBinding;
 import cn.ommiao.mowidgets.databinding.LayoutEdittextBinding;
 import cn.ommiao.mowidgets.utils.SPUtil;
 import cn.ommiao.mowidgets.utils.ToastUtil;
@@ -15,6 +16,9 @@ public class WeatherForecastConfigActivity extends BaseConfigActivity<WeatherFor
 
     private LayoutEdittextBinding keyBinding;
     private String key;
+
+    private LayoutColorSelectorBinding selectorBindingCard1, selectorBindingText1, selectorBindingCard2, selectorBindingText2, selectorBindingCard3, selectorBindingText3;
+    private String colorCard1, colorText1, colorCard2, colorText2, colorCard3, colorText3;
 
     @Override
     protected WeatherForecastWidget getTargetWidget() {
@@ -43,6 +47,18 @@ public class WeatherForecastConfigActivity extends BaseConfigActivity<WeatherFor
                             return result;
                         }});
         addConfigView(keyBinding.getRoot());
+        selectorBindingCard1 = getColorSelectorBinding("天气1卡片颜色", "FFD200");
+        selectorBindingCard2 = getColorSelectorBinding("天气2卡片颜色", "E2E2E2");
+        selectorBindingCard3 = getColorSelectorBinding("天气3卡片颜色", "1C1E21");
+        selectorBindingText1 = getColorSelectorBinding("天气1文字颜色", "000000");
+        selectorBindingText2 = getColorSelectorBinding("天气2文字颜色", "000000");
+        selectorBindingText3 = getColorSelectorBinding("天气3文字颜色", "FFFFFF");
+        addConfigView(selectorBindingCard1.getRoot());
+        addConfigView(selectorBindingCard2.getRoot());
+        addConfigView(selectorBindingCard3.getRoot());
+        addConfigView(selectorBindingText1.getRoot());
+        addConfigView(selectorBindingText2.getRoot());
+        addConfigView(selectorBindingText3.getRoot());
     }
 
     @Override
@@ -62,6 +78,36 @@ public class WeatherForecastConfigActivity extends BaseConfigActivity<WeatherFor
             ToastUtil.shortToast("请输入有效的和风天气Key");
             return false;
         }
+        colorCard1 = selectorBindingCard1.etColor.getText().toString();
+        colorCard2 = selectorBindingCard2.etColor.getText().toString();
+        colorCard3 = selectorBindingCard3.etColor.getText().toString();
+        colorText1 = selectorBindingText1.etColor.getText().toString();
+        colorText2 = selectorBindingText2.etColor.getText().toString();
+        colorText3 = selectorBindingText3.etColor.getText().toString();
+        if(!isColorValid(colorCard1)){
+            ToastUtil.shortToast("请输入正确的颜色值（天气1卡片颜色）");
+            return false;
+        }
+        if(!isColorValid(colorCard2)){
+            ToastUtil.shortToast("请输入正确的颜色值（天气2卡片颜色）");
+            return false;
+        }
+        if(!isColorValid(colorCard3)){
+            ToastUtil.shortToast("请输入正确的颜色值（天气3卡片颜色）");
+            return false;
+        }
+        if(!isColorValid(colorText1)){
+            ToastUtil.shortToast("请输入正确的颜色值（天气1文字颜色）");
+            return false;
+        }
+        if(!isColorValid(colorText2)){
+            ToastUtil.shortToast("请输入正确的颜色值（天气2文字颜色）");
+            return false;
+        }
+        if(!isColorValid(colorText3)){
+            ToastUtil.shortToast("请输入正确的颜色值（天气3文字颜色）");
+            return false;
+        }
         return true;
     }
 
@@ -69,6 +115,12 @@ public class WeatherForecastConfigActivity extends BaseConfigActivity<WeatherFor
     protected void saveConfigs() {
         SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_location", area);
         SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_key", key);
+        SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_color_card1", "#" + colorCard1);
+        SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_color_card2", "#" + colorCard2);
+        SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_color_card3", "#" + colorCard3);
+        SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_color_text1", "#" + colorText1);
+        SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_color_text2", "#" + colorText2);
+        SPUtil.put(getString(R.string.label_weather_forecast) + widgetId + "_color_text3", "#" + colorText3);
     }
 
 }
--- app/src/main/java/cn/ommiao/mowidgets/widgets/list/WeatherForecastService.java
+++ app/src/main/java/cn/ommiao/mowidgets/widgets/list/WeatherForecastService.java
@@ -36,15 +36,15 @@ public class WeatherForecastService extends BaseRemoteViewsService {
             HeWeather6 heWeather6 = HeWeather6.fromJson(data, HeWeather6.class);
             if(heWeather6.getDaily_forecast() != null && heWeather6.getDaily_forecast().size() >= 3){
                 mData.addAll(heWeather6.getDaily_forecast());
-                mData.get(0).setColorCard("#FFD200");
-                mData.get(1).setColorCard("#E2E2E2");
-                mData.get(2).setColorCard("#1C1E21");
-                mData.get(0).setColorText("#000000");
-                mData.get(1).setColorText("#000000");
-                mData.get(2).setColorText("#FFFFFF");
+                mData.get(0).setColorCard(getColorFromSp("_color_card1", "#FFD200"));
+                mData.get(1).setColorCard(getColorFromSp("_color_card2", "#E2E2E2"));
+                mData.get(2).setColorCard(getColorFromSp("_color_card3", "#1C1E21"));
+                mData.get(0).setColorText(getColorFromSp("_color_text1", "#000000"));
+                mData.get(1).setColorText(getColorFromSp("_color_text2", "#000000"));
+                mData.get(2).setColorText(getColorFromSp("_color_text3", "#FFFFFF"));
             } else {
                 DailyForecast dailyForecast = new DailyForecast();
-                dailyForecast.setDate("美好的一天");
+                dailyForecast.setDate("祝你拥有美好的一天");
                 mData.add(dailyForecast);
             }
         }
@@ -59,20 +59,27 @@ public class WeatherForecastService extends BaseRemoteViewsService {
             setColorForText(views, Color.parseColor(forecast.getColorText()), R.id.tv_date, R.id.tv_weather_desc, R.id.tv_sun_desc, R.id.tv_wind_desc);
             String date = forecast.getDate();
             String[] yyyymmdd = date.split("-");
-            String sep = " / ";
-            String month = yyyymmdd[1];
-            String day = yyyymmdd[2];
-            String all = day + sep + month;
-            SpannableString spannableString = new SpannableString(all);
-            int dayLength = day.length();
-            spannableString.setSpan(new AbsoluteSizeSpan(18, true), 0, dayLength, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            views.setTextViewText(R.id.tv_date, spannableString);
-            views.setInt(R.id.iv_mask, "setColorFilter", Color.parseColor(forecast.getColorCard()));
-            String weatherDesc = "白天" + forecast.getCond_txt_d() + "   " + "夜间" + forecast.getCond_txt_n() + "   " + "气温" + forecast.getTmp_min() + "° - " + forecast.getTmp_max() + "°";
-            views.setTextViewText(R.id.tv_weather_desc, weatherDesc);
+            if(yyyymmdd.length == 3){
+                String sep = " / ";
+                String month = yyyymmdd[1];
+                String day = yyyymmdd[2];
+                String all = day + sep + month;
+                SpannableString spannableString = new SpannableString(all);
+                int dayLength = day.length();
+                spannableString.setSpan(new AbsoluteSizeSpan(18, true), 0, dayLength, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                views.setTextViewText(R.id.tv_date, spannableString);
+                views.setInt(R.id.iv_mask, "setColorFilter", Color.parseColor(forecast.getColorCard()));
+                String weatherDesc = "白天" + forecast.getCond_txt_d() + "   " + "夜间" + forecast.getCond_txt_n() + "   " + "气温" + forecast.getTmp_min() + "° - " + forecast.getTmp_max() + "°";
+                views.setTextViewText(R.id.tv_weather_desc, weatherDesc);
+                views.setTextViewText(R.id.tv_sun_desc, "日出" + forecast.getSr() + "   " + "日落" + forecast.getSs());
+                views.setTextViewText(R.id.tv_wind_desc, forecast.getWind_sc() + "级" + forecast.getWind_dir());
+            } else {
+                views.setTextViewText(R.id.tv_date, date);
+                views.setTextViewText(R.id.tv_weather_desc, "心情从早到晚都是晴天");
+                views.setTextViewText(R.id.tv_sun_desc, "日出醒来  日落睡去");
+                views.setTextViewText(R.id.tv_wind_desc, "风平 浪静");
+            }
             views.setImageViewResource(R.id.iv_weather, WeatherUtil.getIconRes(forecast.getCond_code_d()));
-            views.setTextViewText(R.id.tv_sun_desc, "日出" + forecast.getSr() + "   " + "日落" + forecast.getSs());
-            views.setTextViewText(R.id.tv_wind_desc, forecast.getWind_sc() + "级" + forecast.getWind_dir());
             return views;
         }
 
@@ -80,6 +87,14 @@ public class WeatherForecastService extends BaseRemoteViewsService {
             for (int id : ids) {
                 views.setTextColor(id, color);
             }
+        }
+
+        private String getColorFromSp(String key, String defaultValue){
+            String fullKey = mContext.getString(R.string.label_weather_forecast) + widgetId + key;
+            String result = SPUtil.getString(fullKey, defaultValue);
+            Logger.d(fullKey);
+            Logger.d(result);
+            return result;
         }
     }
 
